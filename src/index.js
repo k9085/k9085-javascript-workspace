a336b68006f4b397c07ad835c4672a35345c700b
k9085 <107274302+k9085@users.noreply.github.com>
2022-06-16 20:20:40 +0530

    javascript programs

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
+/*closure example*/
 function hello() {
   var a = 10;
   function hello1() {
@@ -17,3 +18,34 @@ function hello() {
   hello1();
 }
 var hellovalue = hello();
+console.log(hellovalue);
+
+/*reverse string */
+var str = "hi hello world";
+function reverseString(str) {
+  var newValue = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newValue += str[i];
+  }
+  return newValue;
+}
+
+var reverseValue = reverseString(str);
+console.log(reverseValue);
+
+/*double the elements using simple function*/
+const arr = [5, 1, 3, 2, 6];
+
+function double(x) {
+  return x * 2;
+}
+const mapValues = arr.map(double);
+console.log(mapValues);
+
+/*double the elements using map function*/
+
+const arrMap = [5, 1, 3, 2, 6];
+const valuesdouble = arrMap.map((x) => {
+  return x * 2;
+});
+console.log("using map", valuesdouble);
