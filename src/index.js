1722cbca6c57b2a8edf70e5f8f5b1f66d25e280b
k9085 <107274302+k9085@users.noreply.github.com>
2022-06-16 21:09:32 +0530

    javascript programs usiing es6

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function reverseString(str) {
 }
 
 var reverseValue = reverseString(str);
-console.log(reverseValue);
+console.log("reverse the given string --->", reverseValue);
 
 /*double the elements using simple function*/
 const arr = [5, 1, 3, 2, 6];
@@ -49,3 +49,59 @@ const valuesdouble = arrMap.map((x) => {
   return x * 2;
 });
 console.log("using map", valuesdouble);
+
+/*binary values using map */
+
+const binaryArr = [5, 1, 3, 2, 6];
+const binaryVal = binaryArr.map((x) => {
+  return x.toString(2);
+});
+console.log("binary values", binaryVal);
+
+/* filter logic example*/
+/*find even values using filter */
+const arr1 = [5, 1, 3, 2, 6];
+
+const evenValues = arr1.filter((x) => {
+  return x % 2 == 0;
+});
+console.log("even values", evenValues);
+
+/* find odd values using flter*/
+const oddArr = [5, 1, 3, 2, 6];
+const oddValues = oddArr.filter((x) => {
+  return x % 2;
+});
+console.log("odd values", oddValues);
+
+/*greater than 4 using filter */
+const greaterArr = [5, 1, 3, 2, 6];
+const greaterValue = greaterArr.filter((x) => {
+  return x > 4;
+});
+console.log("greater Value", greaterValue);
+
+/*lesser than 4 using filter */
+const lessArr = [5, 1, 3, 2, 6];
+const lesserValue = lessArr.filter((x) => {
+  return x < 4;
+});
+console.log("less value", lesserValue);
+
+/* reduce examples */
+/* sum the array values */
+const sumArr = [5, 1, 3, 2, 6];
+const sumValues = sumArr.reduce((acc, curr) => {
+  return (acc += curr);
+});
+console.log("sum of the array", sumValues);
+
+/* find the largest value */
+const maxValue = [5, 1, 3, 2, 6];
+const largerNumber = maxValue.reduce((max, curr) => {
+  if (curr > max) {
+    max = curr;
+  }
+  return max;
+}, 0);
+console.log("large Number", largerNumber);
